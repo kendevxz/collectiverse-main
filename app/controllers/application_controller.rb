28d83cb83762444e309c6ec327a36534c83f5780
kenny devin <kendevxz@gmail.com>
2023-06-04 22:47:28 +0800

    navbar initialized and login works

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,19 @@ class ApplicationController < ActionController::Base
   before_action :authenticate_user!
   before_action :configure_permitted_parameters, if: :devise_controller?
   skip_before_action :authenticate_user!, only: %i[home about contact]
+
+  def home
+  end
+
+  def about; end
+
+  def contact; end
+
+  def configure_permitted_parameters
+    # For additional fields in app/views/devise/registrations/new.html.erb
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :username])
+
+    # For additional in app/views/devise/registrations/edit.html.erb
+    devise_parameter_sanitizer.permit(:account_update, keys: [:first_name, :last_name, :username])
+  end
 end
