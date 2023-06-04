bf056c5af25088e2275cefe9a72d390e0bc4f5ae
kenny devin <kendevxz@gmail.com>
2023-06-05 00:34:01 +0800

    Navbar, login fixed

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,9 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
   before_action :configure_permitted_parameters, if: :devise_controller?
-  skip_before_action :authenticate_user!, only: %i[home about contact]
+  skip_before_action :authenticate_user!, only: %i[home]
 
-  def home
-  end
+  def home; end
 
   def about; end
 
@@ -12,9 +11,9 @@ class ApplicationController < ActionController::Base
 
   def configure_permitted_parameters
     # For additional fields in app/views/devise/registrations/new.html.erb
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :username])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:username])
 
     # For additional in app/views/devise/registrations/edit.html.erb
-    devise_parameter_sanitizer.permit(:account_update, keys: [:first_name, :last_name, :username])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:username])
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -12,9 +12,14 @@ class User < ApplicationRecord
   # validates :email, presence: true, length: { maximum: 255 },
   #                   format: { with: VALID_EMAIL_REGEX },
   #                   uniqueness: { case_sensitive: false }
-  validates :admin_level, numericality: { only_integer: true,
-                                          less_than_or_equal_to: 3 }
+  # validates :admin_level, numericality: { only_integer: true,
+  #                                         less_than_or_equal_to: 3 }
+  # validates_numericality_of(:admin_level, only_integer: true, less_than_or_equal_to: 3)
 
   before_save { username.downcase! }
   before_save { email.downcase! }
+  before_save :default_user
+  def default_user
+    self.admin_level = 1
+  end
 end
