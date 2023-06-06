db6888f0189b302cd6b8bfceee20326961b8d0b1
Marietta Jecht <mariettaxcvb@gmail.com>
2023-06-06 12:48:33 +0800

    update post controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -8,24 +8,12 @@ class PostsController < ApplicationController
     end
   end
 
-  def create
-    @post = Post.new(post_params)
-    if @post.save
-      redirect_to root_path # adjust later
-    else
-      render :new, status: :unprocessable_entity
-    end
-  end
-
   def destroy
     @post.destroy
     redirect_to root_path, status: :see_other  # adjust later
   end
 
   private
-  def post_params
-   params.require(:post).permit(:title, :content)
-  end
 
   def set_params
     @post = Post.find(params[:id])
