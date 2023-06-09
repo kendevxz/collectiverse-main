4ac01af1f51b51e6278fe5a6b7dc262b52bf2ae2
Jim Witsenboer <jimwitsenboer.dev@gmail.com>
2023-06-09 11:34:04 +0800

    delete old stimulus references from index.js

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -4,9 +4,6 @@
 
 import { application } from "./application"
 
-import CommentsCounterController from "./comments_counter_controller"
-application.register("comments-counter", CommentsCounterController)
-
 import HelloController from "./hello_controller"
 application.register("hello", HelloController)
 
