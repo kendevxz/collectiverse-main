1295a462d126692f26349e3edc47aa689c3b2a71
Jim Witsenboer <jimwitsenboer.dev@gmail.com>
2023-06-24 15:13:38 +0800

    setup pagination for posts on home page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,6 @@
 class PagesController < ApplicationController
   def home
-    @posts = Post.order('karma DESC').all
+    @posts = Post.order('karma DESC').page(params[:page])
     @posts = Post.global_search(params[:query]).order('karma DESC') if params[:query].present?
   end
 end
