28aa4e82ab380e7ee1e701de2f71bd07e6e34a33
Jim Witsenboer <jimwitsenboer.dev@gmail.com>
2023-06-25 11:03:24 +0800

    setup mailtrap

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -81,20 +81,15 @@ Rails.application.configure do
     #   authentication: 'plain',
     #   enable_starttls_auto: true
     # }
+
     config.action_mailer.default_url_options = { :host => 'localhost:3000' }
     config.action_mailer.delivery_method = :smtp
-    # config.action_mailer.perform_deliveries = true
-    # config.action_mailer.raise_delivery_errors = true
-    config.action_mailer.default :charset => "utf-8"
-
-
-    ActionMailer::Base.smtp_settings = {
-      :address => "mail.collectiverse.net",
-    :port => 587,
-    :authentication => :plain,
-    :domain => ENV['SMTP_DOMAIN'],
-    :user_name => ENV['SMTP_USERNAME'],
-    :password => ENV['SMTP_PASSWORD'],
-    enable_starttls_auto: true
-  }
+    config.action_mailer.smtp_settings = {
+      :user_name => ENV['SMTP_USERNAME'],
+      :password => ENV['SMTP_PASSWORD'],
+      :address => ENV['SMTP_ADDRESS'],
+      :host => ENV['SMTP_HOST'],
+      :port => ENV['SMTP_PORT'],
+      :authentication => :cram_md5
+    }
 end
