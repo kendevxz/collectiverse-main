45daeaee900a397f17607735c8146fcd35018ff1
Kenny Devin <kendevxz@gmail.com>
2023-07-04 20:06:28 +0700

    navbar fix

--- db/schema.rb
+++ db/schema.rb
@@ -119,24 +119,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_06_28_033146) do
     t.index ["user_id"], name: "index_subcomments_on_user_id"
   end
 
-  create_table "subscriptions", force: :cascade do |t|
-    t.string "target_type", null: false
-    t.bigint "target_id", null: false
-    t.string "key", null: false
-    t.boolean "subscribing", default: true, null: false
-    t.boolean "subscribing_to_email", default: true, null: false
-    t.datetime "subscribed_at"
-    t.datetime "unsubscribed_at"
-    t.datetime "subscribed_to_email_at"
-    t.datetime "unsubscribed_to_email_at"
-    t.text "optional_targets"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["key"], name: "index_subscriptions_on_key"
-    t.index ["target_type", "target_id", "key"], name: "index_subscriptions_on_target_type_and_target_id_and_key", unique: true
-    t.index ["target_type", "target_id"], name: "index_subscriptions_on_target"
-  end
-
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
