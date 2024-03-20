d3ba05b5070c452e2c0d10ca65a4be99c483c1c5
Kenny Devin <kendevxz@gmail.com>
2024-03-20 16:06:46 +0700

    post controller dried

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,18 +2,19 @@ class CommentsController < ApplicationController
 
   def create
     @post = Post.find(params[:post_id])
-    @comment = Comment.new(comment_params)
-    @comment.post = @post
-    @comment.user = current_user
-
+    @comment = @post.comments.new(comment_params.merge(user: current_user))
     respond_to do |format|
       if @comment.save
-        format.html { redirect_to post_path(@post) }
-        format.json # Follow the classic Rails flow and look for a create.json view
+        format.html { redirect_to @post }
+        format.json { render :show, status: :created, location: @post }
       else
         format.html { render "posts/show", status: :unprocessable_entity }
-        format.json # Follow the classic Rails flow and look for a create.json view
+        format.json { render json: @comment.errors, status: :unprocessable_entity }
       end
+    end
+
+    if @post.save
+      redirect_to post_path(@post)
     end
   end
 
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,28 +1,12 @@
 class PostsController < ApplicationController
-  before_action :set_params, only: %i[index show]
+  before_action :set_post, only: %i[index show upvote downvote]
 
   def upvote
-    @post = Post.find(params[:id])
-    if current_user.voted_for? @post
-      redirect_to posts_path
-    else
-      @post.upvote_by current_user
-      @post.user.increase_karma
-      @post.increase_karma
-      redirect_to posts_path
-    end
+    vote_and_redirect { @post.upvote_by current_user }
   end
 
   def downvote
-    @post = Post.find(params[:id])
-    if current_user.voted_for? @post
-      redirect_to posts_path
-    else
-      @post.downvote_by current_user
-      @post.user.decrease_karma
-      @post.decrease_karma
-      redirect_to root_path
-    end
+    vote_and_redirect { @post.downvote_by current_user }
   end
 
   def new
@@ -30,28 +14,40 @@ class PostsController < ApplicationController
   end
 
   def show
-    @comment = Comment.new  # To be able to create comment on post show page
+    @comment = Comment.new
   end
 
   def create
-    @category = Category.find(params[:post][:category_id].to_i)
-    @post = Post.new(post_params)
-    @post.category_id = @category.id
-    @post.user = current_user
-    if @post.save
-      redirect_to post_path(@post)
-    else
-      render "categories/show", status: :unprocessable_entity
-    end
+    @post = current_user.posts.build(post_params)
+    @post.category_id = params[:post][:category_id].to_i
+    save_and_redirect
   end
 
   private
 
-  def set_params
+  def set_post
     @post = Post.find(params[:id])
   end
 
   def post_params
     params.require(:post).permit(:title, :content, images: [])
+  end
+
+  def vote_and_redirect
+    if current_user.voted_for?(@post)
+      redirect_to posts_path
+    else
+      yield
+      @post.user.send(params[:action] == 'upvote' ? :increase_karma : :decrease_karma)
+      redirect_to posts_path
+    end
+  end
+
+  def save_and_redirect
+    if @post.save
+      redirect_to post_path(@post)
+    else
+      render "categories/show", status: :unprocessable_entity
+    end
   end
 end
